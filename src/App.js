8258144bcfb8ae46d827cd9a53ecdefdccbe037a
Mohamed Hassan <hamobos499@gmail.com>
2021-10-14 23:54:40 +0200

    Initialize App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import './App.css'
+
+function minutesUntilMidnight() {
+  var midnight = new Date();
+  midnight.setHours( 24 );
+  midnight.setMinutes( 0 );
+  midnight.setSeconds( 0 );
+  midnight.setMilliseconds( 0 );
+  return ( midnight.getTime() - new Date().getTime() ) / 1000 / 60;
+}
 
 function App() {
+
+  const [minutesRemaining, setMinutesRemaining] = useState(Math.min(30, minutesUntilMidnight()));
+
+  const handleSession = () => {
+    alert('Session renewed!')
+    setMinutesRemaining(Math.min(30, minutesUntilMidnight()));
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setMinutesRemaining(minutesRemaining - 1);
+    }, 60000); // 1000 * 60 to invoke callback every minute
+    if(Math.floor(minutesRemaining) === -1) {
+      alert('Session ended :( now renewed')
+      setMinutesRemaining(Math.min(30, minutesUntilMidnight()));
+    }
+    return () => clearInterval(interval);
+  }, [minutesRemaining]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Session to expire in {Math.floor(minutesRemaining)} minutes </h3>
+      <br/>
+      <button onClick={handleSession}>Just click me</button>
     </div>
   );
 }
